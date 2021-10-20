98da2cbc6e2f79ac9bfc1160f4036670e652ed7b
Stefan Langerman <stefan.langerman@ulb.ac.be>
2021-10-20 21:18:51 +0200

    changed base url for files to https mirror

--- javascript.js
+++ javascript.js
@@ -4,7 +4,8 @@ var ot_data = {};
 var data_ot9_ready = false;
 var extrem_09_ready = false;
 
-const oturl = "http://www.ist.tugraz.at/aichholzer/research/rp/triangulations/ordertypes/data/";
+//const oturl = "http://www.ist.tugraz.at/aichholzer/research/rp/triangulations/ordertypes/data/";
+const oturl = "http://homepages.ulb.ac.be/~slanger/ot/";
 
 /**
  * Asynchronously loads a portion of the files to search through of the database
